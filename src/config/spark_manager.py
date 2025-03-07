bd036104889b51852347117997ead9432f2d0223
k17hawk <kumardahal536@gmail.com>
2025-03-07 10:05:31 -0500

    docker container updated

--- src/config/spark_manager.py
+++ src/config/spark_manager.py
@@ -1,12 +1,10 @@
 from pyspark.sql import SparkSession
 import os
 
-# Define the path to the JDBC JAR file
 jdbc_jar_path = r"mssql-jdbc-12.8.1.jre11.jar"  
 hadoop_home = r"C:\hadoop\hadoop-3.3.6" 
 os.environ['HADOOP_HOME'] = hadoop_home
 
-# Create a Spark session with the JDBC JAR configured
 spark_session = SparkSession.builder \
     .master('local[*]')\
     .config("spark.driver.memory", "4g")\
